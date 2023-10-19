7302f6638388e965625ee384c4df7cb74af70207
Alex Olazabal Dominguez <alex.o@opendeusto.es>
2023-10-19 18:59:15 +0200

    Implementacion basica de la clase reserva.

--- Proyecto_Prog_III_Hotel/src/Clases/Reserva.java
+++ Proyecto_Prog_III_Hotel/src/Clases/Reserva.java
@@ -1,5 +1,113 @@
 package Clases;
 
+import java.time.LocalDate;
+
 public class Reserva {
 
+	
+	//TODO metodo sacarFactura()
+	
+	protected static int numId;
+	protected int id;
+	protected Cliente cliente;
+	protected LocalDate fechaInicio;
+	protected LocalDate fechaFinal;
+	protected double precioCobrar;
+	protected boolean estaPagado;
+	
+	
+	public Reserva( Cliente cliente, LocalDate fechaInicio, LocalDate fechaFinal, double precioCobrar,
+			boolean estaPagado) {
+		super();
+		numId++;
+		this.id = numId;
+		this.cliente = cliente;
+		this.fechaInicio = fechaInicio;
+		this.fechaFinal = fechaFinal;
+		this.precioCobrar = precioCobrar;
+		this.estaPagado = estaPagado;
+	}
+	
+	
+	public Reserva() {
+		super();
+		numId++;
+		this.id = numId;
+		this.cliente = null;
+		this.fechaInicio = null;
+		this.fechaFinal = null;
+		this.precioCobrar = 0;
+		this.estaPagado = false;
+	}
+
+
+	public static int getNumId() {
+		return numId;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+
+	public LocalDate getFechaFinal() {
+		return fechaFinal;
+	}
+
+
+	public void setFechaFinal(LocalDate fechaFinal) {
+		this.fechaFinal = fechaFinal;
+	}
+
+
+	public double getPrecioCobrar() {
+		return precioCobrar;
+	}
+
+
+	public void setPrecioCobrar(double precioCobrar) {
+		if(precioCobrar>0) {
+			this.precioCobrar = precioCobrar;	
+		}
+		
+	}
+	public boolean isEstaPagado() {
+		return estaPagado;
+	}
+
+
+	public void setEstaPagado(boolean estaPagado) {
+		this.estaPagado = estaPagado;
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Reserva %s, %s, fechaInicio: %s, fechaFinal: %s, %s, pagado? %s", id,
+				cliente, fechaInicio, fechaFinal, precioCobrar, estaPagado);
+	}
+	
+	
+	
 }
